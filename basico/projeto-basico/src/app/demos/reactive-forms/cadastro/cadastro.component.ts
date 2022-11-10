2497a31a776f1c5a67c993c6f6b0e97e565a45c7
Henrique Correa do Amaral <henrique.amaral@rhitmo.com.br>
2022-11-10 16:17:37 -0300

    Feat - binding

--- basico/projeto-basico/src/app/demos/reactive-forms/cadastro/cadastro.component.ts
+++ basico/projeto-basico/src/app/demos/reactive-forms/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
+  cadastroForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+
+    this.cadastroForm = new FormGroup({
+      nome: new FormControl('')
+    })
   }
 
 }
