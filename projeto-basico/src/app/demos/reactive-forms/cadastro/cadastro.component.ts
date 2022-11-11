e782748d2b30c3055822106be660670149eaf74a
Henrique Correa do Amaral <henrique.amaral@rhitmo.com.br>
2022-11-11 14:13:08 -0300

    Feat - Validações de cadastro

--- projeto-basico/src/app/demos/reactive-forms/cadastro/cadastro.component.ts
+++ projeto-basico/src/app/demos/reactive-forms/cadastro/cadastro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from './models/user';
 
 @Component({
@@ -11,23 +11,31 @@ export class CadastroComponent implements OnInit {
 
   signUpForm: FormGroup;
   user: User;
+  formResult: string = ''
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
 
     this.signUpForm = this.formBuilder.group({
-      name: [''],
-      document: [''],
-      email: [''],
+      name: ['', [Validators.required]],
+      document: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
+      email: ['', [Validators.required, Validators.email]],
       password: [''],
       passwordConfirmation: [''],
     })
   }
 
   addUser() {
-    this.user = Object.assign({}, this.user, this.signUpForm.value);
-    console.log(this.user)
+    if (this.signUpForm.valid) {
+      this.user = Object.assign({}, this.user, this.signUpForm.value);
+      this.formResult = JSON.stringify(this.signUpForm.value)
+      console.log(this.user)
+    }
+    else {
+      this.formResult = "Formulário invalido!"
+    }
+
   }
 
 
